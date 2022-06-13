f57bacb8a5a111bd55583e57c080eb12e86240da
acefail <daoxuandat92tn@gmail.com>
2022-06-13 10:00:52 +0700

    Query for Account

--- db/sql/account.sql.go
+++ db/sql/account.sql.go
@@ -34,7 +34,7 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 }
 
 const getAccount = `-- name: GetAccount :one
-SELECT * FROM accounts
+SELECT id, owner, balance, currency, created_at FROM accounts
 WHERE id = $1 LIMIT 1
 `
 
@@ -52,7 +52,7 @@ func (q *Queries) GetAccount(ctx context.Context, id int64) (Account, error) {
 }
 
 const listAccounts = `--name: ListAccounts :many
-SELECT * FROM accounts
+SELECT id, owner, balance, currency, created_at FROM accounts
 ORDER BY id 
 LIMIT $1
 OFFSET $2
@@ -91,4 +91,38 @@ func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]A
 	}
 
 	return items, err
+}
+
+const updateAccount = `-- name: UpdateAccount :exec
+UPDATE accounts
+SET balance = $2
+WHERE id = $1;
+`
+
+type UpdateAccountParams struct {
+	ID      int64 `json:"id"`
+	Balance int64 `json:"balance"`
+}
+
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error) {
+	row := q.db.QueryRowContext(ctx, updateAccount, arg.ID, arg.Balance)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+	)
+	return i, err
+}
+
+const deleteAccount = `--name: DeleteAccount :exec
+DELETE FROM accounts 
+WHERE id = $1;
+`
+
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
+	return err
 }
