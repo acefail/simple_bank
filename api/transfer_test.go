11b007eed9e595fbfd18a4a5e4926e5172cb6dd3
acefail <daoxuandat92tn@gmail.com>
2022-07-01 14:40:26 +0700

    Add JWT and Paseto Token Authentication

--- api/transfer_test.go
+++ api/transfer_test.go
@@ -201,7 +201,7 @@ func TestTransferAPI(t *testing.T) {
 			store := mock_sql.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			// Marshal body data to JSON
---
+++ token/maker.go
@@ -0,0 +1,12 @@
+package token
+
+import "time"
+
+//Maker interface for managing token
+type Maker interface {
+	//CreateToken creates a new token for a specific username and valid duration
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	// VerifyToken checks if a token is valid or not
+	VerifyToken(token string) (*Payload, error)
+}
